be7ecb8aff12773fa90e62eb3215f673d9c8e2f9
Pavel Deshchenia <pavel.deshchenia@gmail.com>
2016-10-30 21:05:10 +0300

    init

---
+++ PerfomanceCounterRegistrar/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using PerformanceCounterConstants;
+using System.Security;
+
+namespace PerfomanceCounterRegistrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (PerformanceCounterCategory.Exists(MusicStorePerformanceConstants.CategoryName))
+                {
+                    PerformanceCounterCategory.Delete(MusicStorePerformanceConstants.CategoryName);
+                }
+
+                CounterCreationDataCollection counters = new CounterCreationDataCollection();
+
+                counters.Add(new CounterCreationData()
+                {
+                    CounterName = MusicStorePerformanceConstants.LoginCounterName,
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                });
+
+                counters.Add(new CounterCreationData()
+                {
+                    CounterName = MusicStorePerformanceConstants.LogoffCounterName,
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                });
+
+                counters.Add(new CounterCreationData()
+                {
+                    CounterName = MusicStorePerformanceConstants.PaymentsCounterName,
+                    CounterType = PerformanceCounterType.NumberOfItems32
+                });
+
+                PerformanceCounterCategory.Create(
+                    MusicStorePerformanceConstants.CategoryName,
+                    string.Empty,
+                    PerformanceCounterCategoryType.SingleInstance,
+                    counters);
+
+                Console.WriteLine($"Perfomance counters added. Category name: {MusicStorePerformanceConstants.CategoryName}");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Console.WriteLine("You don't have needed permissions. Run app as administrator.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
