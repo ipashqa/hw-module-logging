cd0962f3596c9a88fd05205bbbc4b2ced9bc5427
Pavel Deshchenia <pavel.deshchenia@gmail.com>
2016-10-30 23:18:21 +0300

    logparser init

---
+++ LogReader/LogReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MSUtil;
+
+namespace LogReader
+{
+    class LogReader
+    {
+        private string _logFileName = @"..\..\..\MvcMusicStore\logs\2016-10-30.log";
+
+        private LogQueryClass _logQuery;
+        private COMTSVInputContextClass _input;
+
+        public LogReader()
+        {
+            _logQuery = new LogQueryClass();
+
+            //_input = new COMTSVInputContextClass();
+            //_input.headerRow = false;
+        }
+    }
+}
---
+++ LogReader/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
